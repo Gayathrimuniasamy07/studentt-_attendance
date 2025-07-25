682c5246d7ba73365c898b18dd3e37fcd2eb8638
Gayathrimuniasamy07 <gayathrimuniasamy07@gmail.com>
2025-07-25 12:49:19 +0530

    Add backend.js to Backend

---
+++ backend.js
@@ -0,0 +1,16 @@
+const attendanceList = [];
+
+// Function to mark attendance
+function markAttendance(name) {
+  attendanceList.push(name);
+  console.log(`${name} is marked present.`);
+}
+document.getElementById('attendanceForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const studentName = this.student.value.trim();
+  if(studentName) {
+    markAttendance(studentName);
+    alert(`${studentName} attendance marked!`);
+    this.reset();
+  }
+});
